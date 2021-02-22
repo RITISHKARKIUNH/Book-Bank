6fd8058b732cdb841f39cedc8972754ff2aafee2
Ritish Karki <rkark1@unh.newhaven.edu>
2021-02-21 22:02:55 -0500

    first commit for project book bank

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
-import '../styles/globals.css'
+import '../styles/globals.scss';
+//all the imports required for font awesome to work
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
